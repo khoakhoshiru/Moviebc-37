43c53f3b45c48e03ff226a4a2ed38da790ea6b0b
nguyenleanhkhoa <nguyenleanhkhoa857@gmail.com>
2022-12-19 20:31:52 +0700

    update

--- src/features/Booking/components/HomeCarousel.jsx
+++ src/features/Booking/components/HomeCarousel.jsx
@@ -3,14 +3,14 @@ import { Carousel } from "antd";
 import { useSelector } from "react-redux";
 
 const HomeCarousel = () => {
-  const banners = useSelector((state) => {
-    return state.booking.banners;
+  const banners = useSelector(state => {
+    return state.booking?.banners;
   });
 
   return (
     <div>
       <Carousel>
-        {banners.map((item) => {
+        {banners.map(item => {
           return (
             <div key={item.maBanner}>
               <img
